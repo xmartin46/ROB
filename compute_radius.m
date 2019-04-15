d00704731c528a8797190f4e49998dd8ed1d9847
xmartin46 <xmartin46@gmail.com>
2019-04-15 14:27:37 +0200

    trepanation

---
+++ compute_radius.m
@@ -0,0 +1,46 @@
+function [mean_radius, radiusX, radiusY, radiusZ] = compute_radius(tumor)
+    % X
+    x_max = find(tumor(:, 1) == max(tumor(:, 1)));
+    x_min = find(tumor(:, 1) == min(tumor(:, 1)));
+    x = tumor(x_max(3), :);
+    y = tumor(x_min(2), :);
+    x1 = [x(1, 1) y(1, 1)];
+    y1 = [x(1, 2) y(1, 2)];
+    z1 = [x(1, 3) y(1, 3)];
+%     line(x1, y1, z1);
+%     xlabel('X');
+%     ylabel('Y');
+%     zlabel('Z');
+    radiusX = (sqrt(sum((x - y) .^ 2)))/2;
+
+    % Y
+    y_max = find(tumor(:, 2) == max(tumor(:, 2)));
+    y_min = find(tumor(:, 2) == min(tumor(:, 2)));
+    x = tumor(y_max(1), :);
+    y = tumor(y_min(1), :);
+    x1 = [x(1, 1) y(1, 1)];
+    y1 = [x(1, 2) y(1, 2)];
+    z1 = [x(1, 3) y(1, 3)];
+%     line(x1, y1, z1);
+%     xlabel('X');
+%     ylabel('Y');
+%     zlabel('Z');
+    radiusY = (sqrt(sum((x - y) .^ 2)))/2;
+
+    % Z
+    z_max = find(tumor(:, 3) == max(tumor(:, 3)));
+    z_min = find(tumor(:, 3) == min(tumor(:, 3)));
+    x = tumor(z_max(1), :);
+    y = tumor(z_min(1), :);
+    x1 = [x(1, 1) y(1, 1)];
+    y1 = [x(1, 2) y(1, 2)];
+    z1 = [x(1, 3) y(1, 3)];
+%     line(x1, y1, z1);
+%     xlabel('X');
+%     ylabel('Y');
+%     zlabel('Z');
+    radiusZ = (sqrt(sum((x - y) .^ 2)))/2;
+
+    mean_radius = (radiusZ + radiusX + radiusY)/3;
+end
+
---
+++ trepanation.m
@@ -0,0 +1,57 @@
+%%%%%%%%%%%%%%% V1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% tumor_center = mean(tumor(:, :));
+% 
+% % Radi del tumor
+% crani_radi = compute_radius(tumor);
+% 
+% % Ha de ser una mica més petit que el radi del crani
+% trep_radi = crani_radi - 0.0021;
+% 
+% center = tumor_center;
+% 
+% x = 0.5 * cosd(45);
+% z = 0.5 * sind(45);
+% 
+% center = center + ([x-0.05 0 z-0.05] ./ 10);
+% % scatter3(center(:, 1), center(:, 2), center(:, 3), 12, 'k', 'filled')
+% C = h2e(transl(center) *   e2h(roty(45, 'deg') * h2e(transl(-center) * e2h(circle(center, trep_radi, 'N', 200)))))';
+% % scatter3(C(:, 1), C(:, 2), C(:, 3), 12, 'b', 'filled');
+% 
+% for i=1:50
+%     Laser_Pose(:,:,i)= transl(C(i, :)) * troty(-135, 'deg');
+% end
+% 
+% q = p560Craigh.ikine(Laser_Pose, 'run');
+% view(15, 20);
+% axis([0.4, 1.2, -0.4, 0.4, 0.9, 1.7])
+% p560Craigh.plot(q, 'loop', 'fps', 60, 'trail', {'k', 'LineWidth', 2}, 'zoom', 2)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%% V2 (malament) %%%%%%%%%%%%%%%%%%%%%%
+tumor_center = mean(tumor(:, :));
+
+% Radi del tumor
+crani_radi = compute_radius(tumor);
+
+% Ha de ser una mica més petit que el radi del crani
+trep_radi = crani_radi - 0.0021;
+
+center = tumor_center;
+
+x = 0.5;
+
+center = center + ([x-0.05 0 0] ./ 10);
+scatter3(center(:, 1), center(:, 2), center(:, 3), 50, 'k', 'filled')
+C = h2e(transl(center) *   e2h(roty(90, 'deg') * h2e(transl(-center) * e2h(circle(center, trep_radi, 'N', 200)))))';
+scatter3(C(:, 1), C(:, 2), C(:, 3), 12, 'b', 'filled');
+
+for i=1:50
+    Laser_Pose(:,:,i)= transl(C(i, :)) * troty(-135, 'deg');
+end
+
+q = p560Craigh.ikine(Laser_Pose, 'run');
+view(15, 20);
+axis([0.4, 1.2, -0.4, 0.4, 0.9, 1.7])
+p560Craigh.plot(q, 'loop', 'fps', 60, 'trail', {'k', 'LineWidth', 2}, 'zoom', 2)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
